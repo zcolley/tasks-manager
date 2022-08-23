9c8abe713ca3e040a6cadf3536648f8ce001f176
yuhang zhu <zcolley96@gmail.com>
2022-08-22 23:22:51 -0400

    update model

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,8 +1,14 @@
+const tasks = require('../models/tasks')
 const get_all_tasks = (req, res) => {
   res.send('get all tasks')
 }
-const create_tasks = (req, res) => {
-  res.json(req.body)
+const create_tasks = async (req, res) => {
+  try {
+    const task = await tasks.create(req.body)
+    res.status(201).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 const get_task = (req, res) => {
   res.send('get task')
