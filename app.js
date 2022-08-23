dfd79320ed20f10c09be7d0bee3a96aa350cee67
yuhang zhu <zcolley96@gmail.com>
2022-08-22 22:49:21 -0400

    add env

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+const connect_db = require('./db/connect')
 const express = require('express')
 const tasks = require('./routes/tasks')
 const app = express()
-
+require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
@@ -11,4 +12,13 @@ app.get('/', (req, res) => {
 app.use('/api/v1/tasks', tasks)
 const PORT = 3000
 
-app.listen(PORT, () => console.log('Server is listening on port ' + PORT))
+const start = async () => {
+  try {
+    await connect_db(process.env.MONGO_URL)
+    app.listen(PORT, () => console.log('Server is listening on port ' + PORT))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+start()
